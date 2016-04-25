4e699ce9212b790db69dcbe20e5bcfe01e393d6a
Thomas Hourlier <thomas.hourlier@cnode.fr>
2016-04-25 10:34:22 -0700

    Increase the coverage to 100%.
    Just to have a green badge! These tests for 'retax' are not really revelant. All sub retax modules are already tested.

--- src/__tests__/server-test.ts
+++ src/__tests__/server-test.ts
@@ -3,7 +3,12 @@
 import middleware from '../server';
 
 describe('retax server', () => {
+  const serverConfig = {
+    serverRendering: true,
+  };
+
   it('exposes the middleware', () => {
     expect(middleware instanceof Function).toBeTruthy();
+    expect(middleware(serverConfig) instanceof Function);
   });
 });
