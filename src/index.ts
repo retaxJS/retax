07ea5fa738dfad94f74c29d83ac460e3145f02e8
Thomas Hourlier <thomas.hourlier@cnode.fr>
2016-04-25 09:14:35 -0700

    Expose the app kernel to allow modules to register.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+export { default as retaxKernel } from './di';
 export { default as retax } from './client';
 export { default as retaxMiddleware } from './server';
 
 export { actionsCreatorFactory, reducerFactory } from 'retax-utils';
 export { setAuthToken, removeAuthToken } from 'retax-core';
-
